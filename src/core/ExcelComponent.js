e05df9a901fd1acb9406e43b968194f3bd60ed73
sebii_sw <bilasebastyan@gmail.com>
2020-08-15 13:13:24 +0300

    Finish base framework setup

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,21 @@
+import {DOMListener} from '@core/DOMListener';
+
+export class ExcelComponent extends DOMListener {
+    constructor($root, options = {}) {
+        super($root, options.listeners)
+        this.name = options.name || ''
+    }
+
+    // Return the template of html
+    toHTML() {
+        return ''
+    }
+
+    init() {
+        this.initDOMListeners()
+    }
+
+    destroy() {
+        this.removeDOMListeners()
+    }
+}
